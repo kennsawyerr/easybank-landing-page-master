ee0d0e6d5ddb3e3dfacbabf6daa80c0b5474c5a6
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-07-21 23:37:10 +0100

    Changes to be committed: 	modified:   index.html 	modified:   index.js 	modified:   style.css

---
+++ index.js
@@ -0,0 +1,8 @@
+const navBtn = document.getElementById("navBtn");
+
+
+const menuBtn = document.getElementById("menuBtn")
+
+menuBtn.addEventListener("click", function(){
+   navBtn.classList.toggle("active");
+})
