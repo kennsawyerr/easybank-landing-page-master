98810fdd2377cedccc13430614e389623aa7feb1
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-08-04 13:22:26 +0100

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const navBtn = document.getElementById("navBtn");
-
+const sections=document.querySelectorAll(".section")
 
 const menuBtn = document.getElementById("menuBtn")
 
 menuBtn.addEventListener("click", function(){
    navBtn.classList.toggle("active");
-   navBtn.classList.toggle("focused");
+   // sections.forEach(function(section) {
+   //    section.classList.toggle("focused");})
+   body.classList.toggle("focused")
 
 })
+
+
