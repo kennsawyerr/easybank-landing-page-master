049a3968f3662e9c6a0f1631326108c244721f80
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-08-18 06:58:12 +0100

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const navBtn = document.getElementById("navBtn");
-const sections=document.querySelectorAll(".section")
+const sections = document.querySelectorAll(".section")
 
 const overlay = document.querySelector(".overlay")
 const menuBtn = document.getElementById("menuBtn")
 
-menuBtn.addEventListener("click", function(){
+menuBtn.addEventListener("click", function () {
    navBtn.classList.toggle("active");
    overlay.style.visibility = "visible";
 })
 
-window.addEventListener("click",function(event){
-   if(event.target != menuBtn){navBtn.classList.remove("active");
-   overlay.style.visibility = "hidden";
+window.addEventListener("click", function (event) {
+   if (event.target != menuBtn) {
+      navBtn.classList.remove("active");
+      overlay.style.visibility = "hidden";
 
-}
+   }
 
-} )
-
+})
